2e1c5c2b785f48db98cae17c82a8dc6606dca20e
Donald Whitely <dswhitely1@gmail.com>
2019-10-20 18:01:47 -0400

    Added Auth Routes

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -1,5 +1,7 @@
 const testRouter = require('./test/test.router');
+const authRouter = require('./auth/auth.router');
 
 module.exports = server => {
   server.use('/', testRouter);
+  server.use('/api/auth', authRouter);
 };
