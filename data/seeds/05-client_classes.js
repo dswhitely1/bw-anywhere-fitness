62fa5ae4eb78fc92a07739405eec85359a1198f4
Donald Whitely <dswhitely1@gmail.com>
2019-10-21 08:45:46 -0400

    ReAdded Seed Data

---
+++ data/seeds/05-client_classes.js
@@ -0,0 +1,14 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('class_clients')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('class_clients').insert([
+        { classId: 1, clientId: 2 },
+        { classId: 1, clientId: 3 },
+        { classId: 2, clientId: 2 },
+        { classId: 2, clientId: 4 },
+      ]);
+    });
+};
