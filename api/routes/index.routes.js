05dc5a9ae5a2d76bbdbc0ece666d51f17d30714c
Donald Whitely <dswhitely1@gmail.com>
2019-10-20 21:45:13 -0400

    Added Category Router to Index

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -1,9 +1,11 @@
 const testRouter = require('./test/test.router');
 const authRouter = require('./auth/auth.router');
 const userRouter = require('./user/user.router');
+const categoryRouter = require('./categories/category.router');
 
 module.exports = server => {
   server.use('/', testRouter);
   server.use('/api/auth', authRouter);
   server.use('/api/user', userRouter);
+  server.use('/api/category', categoryRouter);
 };
