6bf2a2368172b5099bacf393e18359b202e81dfe
Donald Whitely <dswhitely1@gmail.com>
2019-10-20 22:28:37 -0400

    Added Classes Routes to Index

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -2,10 +2,12 @@ const testRouter = require('./test/test.router');
 const authRouter = require('./auth/auth.router');
 const userRouter = require('./user/user.router');
 const categoryRouter = require('./categories/category.router');
+const classesRouter = require('./classes/classes.router');
 
 module.exports = server => {
   server.use('/', testRouter);
   server.use('/api/auth', authRouter);
   server.use('/api/user', userRouter);
   server.use('/api/category', categoryRouter);
+  server.use('/api/classes', classesRouter);
 };
