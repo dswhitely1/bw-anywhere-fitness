d2f1f6855cb7efe069b7275dabef8baf4ef5c149
Donald Whitely <dswhitely1@gmail.com>
2019-10-21 08:50:38 -0400

    Removed AfterCreate

--- knexfile.js
+++ knexfile.js
@@ -14,9 +14,6 @@ module.exports = {
     pool: {
       min: 2,
       max: 10,
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
     },
     migrations: {
       directory: './data/migrations',
@@ -32,9 +29,6 @@ module.exports = {
     pool: {
       min: 2,
       max: 10,
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
     },
     useNullAsDefault: true,
     migrations: {
