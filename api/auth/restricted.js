d271b473d14efb37da04884ffff5b047a5c5b7fb
Donald Whitely <dswhitely1@gmail.com>
2019-10-20 20:29:04 -0400

    Fixed Restricted Middleware

--- api/auth/restricted.js
+++ api/auth/restricted.js
@@ -1,15 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.SECRET;
-
 module.exports = (req, res, next) => {
-  const token = req.headers.authentication;
+  const token = req.headers.authorization;
+  const secret = process.env.SECRET_OR_KEY;
   if (token) {
     jwt.verify(token, secret, (err, decodeToken) => {
       if (err) {
         res.status(401).json({ message: 'Not Authorized' });
       } else {
-        req.user = { id: decodeToken.id, roleId: decodeToken.roleId };
+        req.user = decodeToken;
         next();
       }
     });
