08a145d899061a75f468f7443e2d475cf7a50c28
Donald Whitely <dswhitely1@gmail.com>
2019-10-20 20:30:20 -0400

    Added UserRouter to the Index

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -1,7 +1,9 @@
 const testRouter = require('./test/test.router');
 const authRouter = require('./auth/auth.router');
+const userRouter = require('./user/user.router');
 
 module.exports = server => {
   server.use('/', testRouter);
   server.use('/api/auth', authRouter);
+  server.use('/api/user', userRouter);
 };
